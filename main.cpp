629a24f94cb3083e580be11e1617982b65bc9e65
qm210 <quantenmace@gmail.com>
2021-02-27 02:03:55 +0100

    tryin to do good..

--- main.cpp
+++ main.cpp
@@ -127,6 +127,19 @@ int main(int argc, char *argv[])
 
     glClearColor(backgroundR, backgroundG, backgroundB, 1.0f);
 
+    //// qm: here
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version.c_str());
 
